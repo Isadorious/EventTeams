19c7a734c4a72f0f5a6cc7b0ed91a8a7059f152c
Isadorious <47084294+Isadorious@users.noreply.github.com>
2019-10-01 11:18:13 +0100

    Allow factions to be joined that aren't set to accept everyone

--- EventTeams/EventTeamsCommands.cs
+++ EventTeams/EventTeamsCommands.cs
@@ -60,21 +60,12 @@ namespace EventTeams
 
                 IMyFaction targetFaction = MyAPIGateway.Session.Factions.TryGetFactionByTag(minFactionPair.Key);
 
-                if (targetFaction.AutoAcceptMember)
-                {
-                    MyAPIGateway.Session.Factions.SendJoinRequest(targetFaction.FactionId, Context.Player.IdentityId); // Adds player to faction
+                MyAPIGateway.Session.Factions.SendJoinRequest(targetFaction.FactionId, Context.Player.IdentityId); // Adds player to faction
+                MyAPIGateway.Session.Factions.AcceptJoin(targetFaction.FactionId, Context.Player.IdentityId); // Accept player to faction
 
-                    // Report back to player
-                    Context.Respond("Added to faction: " + minFactionPair.Key);
-                    Plugin.Logger.Info("Assigned " + Context.Player.DisplayName + " to faction " + targetFaction.Tag);
-                }
-                else
-                {
-                    Context.Respond("Unable to assign faction, not set to accept everyone");
-                    Plugin.Logger.Warn(targetFaction.Tag + " not set to accept everyone");
-                }
-
-               
+                // Report back to player
+                Context.Respond("Added to faction: " + minFactionPair.Key);
+                Plugin.Logger.Info("Assigned " + Context.Player.DisplayName + " to faction " + targetFaction.Tag);
 
 
             }
